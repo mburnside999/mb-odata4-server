e696af7ab9c111863309d4f32e11d4b68551cccb
Gabe Sumner <gsumner@gsumner-ltm8.internal.salesforce.com>
2020-07-02 17:30:02 -0700

    First commit.

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const app = express()
+var ODataServer = require('simple-odata-server')
+var Adapter = require('simple-odata-server-nedb')
+var Datastore = require('nedb')
+var db = new Datastore({ inMemoryOnly: true })
+const PORT = process.env.PORT || 5000
+
+var model = {
+  namespace: 'jsreport',
+  entityTypes: {
+    'UserType': {
+      '_id': {'type': 'Edm.String', key: true},
+      'test': {'type': 'Edm.String'},
+      'num': {'type': 'Edm.Int32'},
+      'd': {'type': 'Edm.DateTimeOffset'},
+      'addresses': { 'type': 'Collection(jsreport.AddressType)' }
+    }
+  },
+  complexTypes: {
+    'AddressType': {
+      'street': {'type': 'Edm.String'}
+    }
+  },
+  entitySets: {
+    'users': {
+      entityType: 'jsreport.UserType'
+    }
+  }
+}
+
+var odataServer = ODataServer()
+  .model(model)
+  .adapter(Adapter(function (es, cb) { cb(null, db) }))
+
+app.use("/", function (req, res) {
+    odataServer.handle(req, res);
+});
+
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+db.insert({'_id': '1', 'test': 'a', num: 1, addresses: [{'street': 'a1'}]})
+db.insert({'_id': '2', 'test': 'b', num: 2, addresses: [{'street': 'a2'}]})
+db.insert({'_id': '3', 'test': 'c', num: 3})
+db.insert({'_id': '4', 'test': 'd', num: 4})
+db.insert({'_id': '5', 'test': 'e', num: 5})
