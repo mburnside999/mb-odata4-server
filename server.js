e487668586385ebac8a1c4159e5b05b59b36962d
Mike Burnside <mburnside@salesforce.com>
2023-06-16 14:29:52 +1000

    Test

--- server.js
+++ server.js
@@ -42,17 +42,8 @@ var odataServer = ODataServer()
     })
   );
 
-var odataServer1 = ODataServer()
-  .model(model)
-  .adapter(
-    Adapter(function (es, cb) {
-      cb(null, db1);
-    })
-  );
-
 app.use("/", function (req, res) {
   odataServer.handle(req, res);
-  odataServer1.handle(req, res);
 });
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
