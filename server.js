2d39563bc3dd3bc0ad0599ed76dd9e52fcfa4a1f
Mike Burnside <mburnside@salesforce.com>
2023-06-16 14:56:26 +1000

    Test

--- server.js
+++ server.js
@@ -15,20 +15,13 @@ var model = {
       productname: { type: "Edm.String" },
       productdescription: { type: "Edm.String" },
       productstatus: { type: "Edm.String" },
-    },
-    ProductAccessoryMilestonesType: {
-      _id: { type: "Edm.String", key: true },
-      productid: { type: "Edm.String" },
-      statusdate: { type: "Edm.Date" },
-      status: { type: "Edm.String" },
+      productreleasedate: { type: "Edm.Date" },
+      productversion: { type: "Edm.String" },
     },
   },
   entitySets: {
     productaccessory: {
       entityType: "jsreport.ProductAccessoryType",
-    },
-    productaccessorymilestones: {
-      entityType: "jsreport.ProductAccessoryMilestonesType",
     },
   },
 };
@@ -50,68 +43,36 @@ app.listen(PORT, () => console.log(`Listening on ${PORT}`));
 //insert product accessories and milestones
 
 db.insert({
-  productaccessory: {
-    _id: "VX1-12-222",
-    productname: "VoltX1",
-    productdescription: "Mobile Phone Holder",
-    productstatus: "Available",
-  },
+  _id: "VX1-12-222",
+  productname: "VoltX1",
+  productdescription: "Mobile Phone Holder",
+  productstatus: "Available",
+  productreleasedate: "2020-12-12",
+  productversion: "1.02",
 });
 
-// db.insert("productaccessorymilestones", {
-//   _id: "1",
-//   productid: "VX1-12-222",
-//   statusdate: "2020-12-12",
-//   status: "Alpha",
-// });
-// db.insert("productaccessorymilestones", {
-//   _id: "2",
-//   productid: "VX1-12-222",
-//   statusdate: "2021-05-29",
-//   status: "Beta",
-// });
-// db.insert("productaccessorymilestones", {
-//   _id: "3",
-//   productid: "VX1-12-222",
-//   statusdate: "2022-12-12",
-//   status: "GA",
-// });
+db.insert("productaccessory", {
+  _id: "VX1-10-222",
+  productname: "VoltX1",
+  productdescription: "Pump Bracket",
+  productstatus: "Available",
+  productreleasedate: "2020-12-12",
+  productversion: "1.02",
+});
 
-// db.insert("productaccessory", {
-//   _id: "VX1-10-222",
-//   productname: "VoltX1",
-//   productdescription: "Pump Bracket",
-//   productstatus: "Available",
-// });
-// db1.insert({
-//   _id: "1",
-//   productid: "VX1-10-222",
-//   statusdate: "2021-06-18",
-//   status: "GA",
-// });
-
-// db.insert("productaccessory", {
-//   _id: "VX1-10-009",
-//   productname: "VoltX1",
-//   productdescription: "Rear Light",
-//   productstatus: "Available",
-// });
-// db1.insert({
-//   _id: "1",
-//   productid: "VX1-10-009",
-//   statusdate: "2021-06-18",
-//   status: "GA",
-// });
-
-// db.insert("productaccessory", {
-//   _id: "VX1-10-010",
-//   productname: "VoltX1",
-//   productdescription: "Front Light",
-//   productstatus: "Available",
-// });
-// db.insert("productaccessory", {
-//   _id: "VX1-99-990",
-//   productname: "VoltX1",
-//   productdescription: "Racing Decal",
-//   productstatus: "Back Order",
-// });
+db.insert("productaccessory", {
+  _id: "VX1-10-010",
+  productname: "VoltX1",
+  productdescription: "Front Light",
+  productstatus: "Available",
+  productreleasedate: "2020-12-12",
+  productversion: "1.02",
+});
+db.insert("productaccessory", {
+  _id: "VX1-99-990",
+  productname: "VoltX1",
+  productdescription: "Racing Decal",
+  productstatus: "Back Order",
+  productreleasedate: "2020-12-12",
+  productversion: "1.02",
+});
